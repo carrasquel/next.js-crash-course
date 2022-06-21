948d44bdad23b547ab073f64decd61ba433dbe05
Nelson Carrasquel <carrsquel@outlook.com>
2022-06-21 11:37:06 -0400

    basic spa

--- nextjs-course/pages/news/index.js
+++ nextjs-course/pages/news/index.js
@@ -1,5 +1,24 @@
+import Link from 'next/link';
+import { Fragment } from 'react';
+
 function NewsPage(){
-    return <h1>The News Page</h1>
+    return (
+        <Fragment>
+            <h1>The News Page</h1>
+            <ul>
+                <li>
+                    <Link href="/news/next-js-is-great">
+                        NextJs is a Great Framework
+                    </Link>
+                </li>
+                <li>
+                    <Link href="/news/something-else">
+                        Something else
+                    </Link>
+                </li>
+            </ul>
+        </Fragment>
+    )
 }
 
 export default NewsPage;
