7b6617c4c2c1c3050b25f9442dcb5fbdf9b1bd0b
Nelson Carrasquel <carrsquel@outlook.com>
2022-06-22 00:43:01 -0400

    static props

--- nextjs-main/pages/index.js
+++ nextjs-main/pages/index.js
@@ -1,3 +1,5 @@
+import { useEffect, useState } from 'react';
+
 import Layout from "../components/layout/Layout";
 import MeetupList from "../components/meetups/MeetupList";
 
@@ -13,17 +15,40 @@ const DUMMY_MEETUPS = [
         id: 2,
         title: "Second Meetup",
         image: "https://upload.wikimedia.org/wikipedia/commons/thumb/8/8a/Panoramic_view_of_Caracas%2C_Venezuela_1900_restored_version.jpg/1280px-Panoramic_view_of_Caracas%2C_Venezuela_1900_restored_version.jpg",
-        address: "C.C. San Ignacio",
-        description: "This is a first meetup!"
+        address: "C.C. Sambil",
+        description: "This is a second meetup!"
     },
 ]
 
-function HomePage() {
- return (
-    <MeetupList
-        meetups={DUMMY_MEETUPS}
-    />
+function HomePage(props) {
+    return (
+        <MeetupList
+            meetups={props.meetups}
+        />
  )
 }
+
+export async function getServerSideProps(context) {
+    // fetch data anyhow
+
+    const req = context.req;
+    const res = context.resp;
+
+    return {
+        props: {
+            meetups: DUMMY_MEETUPS
+        }
+    }
+}
+
+// export async function getStaticProps() {
+//     // fetch data anyhow
+//     return {
+//         props: {
+//             meetups: DUMMY_MEETUPS
+//         },
+//         revalidate: 20
+//     }
+// }
 
 export default HomePage;
--- nextjs-main/pages/meetups/[meetupId].js
+++ nextjs-main/pages/meetups/[meetupId].js
@@ -1,16 +1,45 @@
 import MeetupDetails from "../../components/meetups/MeetupDetails";
 
-function MeetupDetailsPage(){
+function MeetupDetailsPage(props){
 
-    const meetupDetails = {
-        id: 1,
-        title: "First Meetup",
-        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/8/8a/Panoramic_view_of_Caracas%2C_Venezuela_1900_restored_version.jpg/1280px-Panoramic_view_of_Caracas%2C_Venezuela_1900_restored_version.jpg",
-        address: "C.C. San Ignacio",
-        description: "This is a first meetup!"
-    };
+    const meetupDetails = props.meetup;
 
     return <MeetupDetails {...meetupDetails}></MeetupDetails>;
+}
+
+export async function getStaticPaths(){
+    // fetch ids anyhow and generate
+    return {
+        paths: [
+            {
+                params: {
+                    meetupId: "1"
+                }
+            },
+            {
+                params: {
+                    meetupId: "2"
+                }
+            }
+        ],
+        fallback: false
+    }   
+}
+
+export async function getStaticProps(context){
+    const meetupId = context.params.meetupId;
+    console.log(meetupId);
+    return {
+        props: {
+            meetup: {
+                id: 1,
+                title: "First Meetup",
+                image: "https://upload.wikimedia.org/wikipedia/commons/thumb/8/8a/Panoramic_view_of_Caracas%2C_Venezuela_1900_restored_version.jpg/1280px-Panoramic_view_of_Caracas%2C_Venezuela_1900_restored_version.jpg",
+                address: "C.C. San Ignacio",
+                description: "This is a first meetup!"
+            }
+        }
+    }
 }
 
 export default MeetupDetailsPage;
