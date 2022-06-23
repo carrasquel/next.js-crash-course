0ac76e8b326ca861e367cc5ffbc3835bcd173662
Nelson Carrasquel <carrsquel@outlook.com>
2022-06-22 20:00:59 -0400

    basic api fetch

--- nextjs-main/pages/new-meetup.js
+++ nextjs-main/pages/new-meetup.js
@@ -1,9 +1,22 @@
+import { useRouter } from 'next/router';
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
 import Layout from "../components/layout/Layout";
 
 function NewMeetup(){
-    function addMeetupHandler(meetupData){
+    const router = useRouter();
+    async function addMeetupHandler(meetupData){
         console.log(meetupData);
+        const response = await fetch('/api/new-meetup', {
+            method: 'POST',
+            body: JSON.stringify(meetupData),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+
+        const data = await response.json();
+        console.log(data);
+        router.push('/');
     };
 
     return (
